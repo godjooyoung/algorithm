0dbd1a39dd2bbcc7b1a7b4794a7fc63797fcef9b
신주영 <58963027+godjooyoung@users.noreply.github.com>
2023-04-10 17:31:33 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 2.87 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/README.md
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 다루기 기본 - 12918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 2.49 ms
+메모리: 33.7 MB, 시간: 2.87 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,8 +1,7 @@
 function solution(s) {
     // 길이 확인 (4또는 6)
-    // 전부 숫자인지 확인 (나누었을때 NaN이 나오면 숫자가 아님, NaN은 Falsy한 값)
+    // 전부 숫자인지 확인 (나누었을때 NaN이 나오면 숫자가 아님, NaN은 Falsy한 값) isNaN 숫자면 false 숫자가 아니면 true
     console.log("숫자 판단1 : ",typeof (+"1e22")) // 1e22, 알파벳 e가 포함되어서 지수형으로 판단 가능한 문자열이 들어오면 문제가 생김
-    
     if(s.length === 4 || s.length === 6){
         let strArr = [...s].filter(function(x){
             return isNaN(x/1)
@@ -17,5 +16,4 @@ function solution(s) {
     }else{
         return false
     }
-    return (s.length === 4 || s.length === 6)?(((typeof s/0) == Number?true:false)?true:false):false;
 }
