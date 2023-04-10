e54e34af5f7e4ede1e8f447c86b394d9fbbe4a12
신주영 <58963027+godjooyoung@users.noreply.github.com>
2023-04-10 15:46:45 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -2,7 +2,7 @@ function solution(a, b) {
     let dayOfWeek = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
     let month = a
     let day = b
-    let curiousDate = new Date('2016-'+month+'-'+day)
+    let curiousDate = new Date('2016-'+month+'-'+day) // Date객체, getDay() 메서드 사용시 0(SUN)~6(SAT) 리턴 
 
     return dayOfWeek[curiousDate.getDay()];
 }
