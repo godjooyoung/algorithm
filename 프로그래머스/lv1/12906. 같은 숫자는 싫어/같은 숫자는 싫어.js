1becf01edafbef6eb8cd4cd0c0b327d6796a845c
신주영 <58963027+godjooyoung@users.noreply.github.com>
2023-04-11 10:25:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 63.78 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,12 @@
+function solution(arr){
+    // 스택으로 구현
+    let answer = [];
+    arr.map((x)=>{
+    // pop을 사용하면 마지막 값만 가져오지만 삭제되므로
+    // 전체 길이에서 -1한 인덱스 값으로 저장된 값을 읽어온다.
+        if(answer[answer.length-1] !== x){
+            answer.push(x)
+        }
+    })
+    return answer;
+}
