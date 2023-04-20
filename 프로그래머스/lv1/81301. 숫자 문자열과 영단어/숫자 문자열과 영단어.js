820d8eb97882bcca8da27c24e3d9fc3a4c7e9a38
신주영 <58963027+godjooyoung@users.noreply.github.com>
2023-04-20 20:09:47 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,11 +1,16 @@
 function solution(s) {
-    const matchEng = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven','eight', 'nine']
     
-    while(isNaN(s/1)){
+    const matchEng = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven','eight', 'nine']
+    /* 
+        isNaN(값) 
+        값이 숫자면 false/ 값이 숫자가 아니면 true return
+     */
+    
+     while(isNaN(s/1)){
         matchEng.map((x,i)=>{
             s = s.replace(x,i)
         })
-    }
+     }
     
     return Number(s);
 }
