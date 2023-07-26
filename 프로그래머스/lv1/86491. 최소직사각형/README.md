f9cb7365ea293785619c5912e26a1600e2523b2f
신주영 <58963027+godjooyoung@users.noreply.github.com>
2023-07-27 02:21:46 +0900

    [level 1] Title: 최소직사각형, Time: 11.29 ms, Memory: 39.5 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/README.md
+++ 프로그래머스/lv1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 39.1 MB, 시간: 9.92 ms
+메모리: 39.5 MB, 시간: 11.29 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,46 +1,22 @@
 function solution(sizes) {
-    var answer = 0;
-    var longArr= new Array;     // 긴 면의 길이를 담는 배열
-    var shortArr= new Array;    // 짧은 면의 길이를 담는 배열
-    var lMax;   // 긴 면의 길이 중 가장 큰 값
-    var sMax;   // 작은 면의 길이 중 가장 큰 값
-    let wTemp;
-    let hTemp;
-    for(i=0; i<sizes.length; i++){
-        wTemp = sizes[i][0];
-        hTemp = sizes[i][1];
-        if(wTemp > hTemp){ 
-longArr.push(wTemp)         
-shortArr.push(hTemp)         
-}else{
-longArr.push(hTemp)
-shortArr.push(wTemp)
-}
-}
+	let longestLengths = []; 	// 변수명 수정. 긴 면의 길이를 담는 배열
+	let shortestLengths = [];	// 변수명 수정. 짧은 면의 길이를 담는 배열
+  
+  for (let i = 0; i < sizes.length; i++) {
+    let [w, h] = sizes[i];		// 구조분해할당 사용
+    if (w > h) {				
+      longestLengths.push(w);
+      shortestLengths.push(h);
+    } else {
+      longestLengths.push(h);
+      shortestLengths.push(w);
+    }
+  }
 
-longArr.sort(function(a,b){
-if(a>b){
-return 1
-}
-if(a<b){
-    return -1
-}
-            
-            return 0})
-shortArr.sort(function(a,b){
-if(a>b){
-return 1
-}
-if(a<b){
-    return -1
-}
-            
-            return 0})
-let endIdx = sizes.length-1
-    
-lMax =longArr[endIdx]
-sMax = shortArr[endIdx]
-   
-answer = lMax*sMax
-    return answer
+  longestLengths.sort((a, b) => b - a);	// 비교함수 추가. 화살표 함수. a,b를 매개벼수로 받아서 차를 리턴한다.
+  shortestLengths.sort((a, b) => b - a);
+  // 자바스크립트 sort 메서드의 비교함수는 리턴값이 음수이면 첫번째 매개변수가 두번째 매개변수보다 작다고 판단. 
+  // 만약 리턴값이 양수이면 첫번째 매개변수가 두번째 매개변수보다 크다고 판단.
+
+  return longestLengths[0] * shortestLengths[0];
 }
