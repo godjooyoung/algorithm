a930f7fbdadf8abea345ce3457c51524a6a4499a
신주영 <58963027+godjooyoung@users.noreply.github.com>
2023-08-14 21:19:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 34.81 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,14 +1,18 @@
-function solution(arr){
-    // 스택으로 구현
-    let answer = [];
-    arr.map((x)=>{
-    // pop을 사용하면 마지막 값만 가져오지만 삭제되므로
-    // 전체 길이에서 -1한 인덱스 값으로 저장된 값을 읽어온다.
-        if(answer[answer.length-1] !== x){
-            answer.push(x)
-        }
-    })
-    return answer;
+// function solution(arr){
+//     // 스택으로 구현
+//     let answer = [];
+//     arr.map((x)=>{
+//     // pop을 사용하면 마지막 값만 가져오지만 삭제되므로
+//     // 전체 길이에서 -1한 인덱스 값으로 저장된 값을 읽어온다.
+//         if(answer[answer.length-1] !== x){
+//             answer.push(x)
+//         }
+//     })
+//     return answer;
+// }
+
+
+function solution(arr) {
+  const set = new Set(arr);  // 중복을 제거한 셋(Set) 생성
+  return [...set];  // 셋(Set)을 다시 배열로 변환하여 반환
 }
-
-
